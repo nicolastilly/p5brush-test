8b369e415125fefb9d7424a2dcce09998ccb245f
Nicolas Tilly <underworld001@hotmail.fr>
2026-04-17 12:44:38 +0200

    initial update

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    background("#f6f1e8");
+
+
+    brush.scaleBrushes(3);
+
+    brush.set("rotring", "#181C14", 0.8);
+    brush.noFill();
+    brush.hatch(7, 15);
+
+    brush.field("zigzag");
+
+    brush.rect(10, 10, width - 20, 700 - 20, "center");
+
+
+    brush.fill("#002185", 175);
+    brush.circle(10, 20, 150, 0.3);
+}
+
+
+
+function draw() {
+
+    //frameRate(10);
+    // brush.line(15, 10, 200, 10);
+
+
+}
